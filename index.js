3e8c63a6f5b2df4d0c9fd4fe3cdbbb438dcf5e70
cypriankanda <cyprianboit@gmail.com>
2025-03-14 23:55:39 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    // Grab the button element using its ID
+    const input = document.getElementById('button');
+    
+    // Add an event listener to the button for the 'click' event
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+  }
+  
+  // Call the function to activate the event listener
+  addingEventListener();
+  
